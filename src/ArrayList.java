23a986730454ca14d031ee4b9ed9b443ee085c3c
Dee-dayo <darhyor2050@gmail.com>
2024-02-08 06:10:21 +0100

    my arraylist test

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class ArrayList {
+public class ArrayList {
+    private boolean isEmpty = true;
+    private int numberOfElements;
+
+    private String[] elements = new String[3];
+
+    public boolean isEmpty() {
+        return numberOfElements == 0;
+    }
+
+    public void add(String element) {
+        if (numberOfElements == elements.length) {
+            String[] temp = new String[elements.length * 2];
+            int tempCounter = 0;
+
+            for (int i = 0; i < elements.length; i++){
+                temp[tempCounter] = elements[i];
+                tempCounter++;
+            }
+            temp[tempCounter] = element;
+            elements = temp;
+
+        }
+
+        elements[numberOfElements] = element;
+        numberOfElements++;
+        isEmpty = false;
+    }
+
+    public void remove(String element) {
+        if (numberOfElements == 0) throw new ArrayIndexOutOfBoundsException("Out of Bound");
+
+        String[] temp = new String[elements.length];
+        int tempCounter = 0;
+
+        for (int index = 0; index < numberOfElements; index++) {
+            if (elements[index].equals(element)) continue;
+            temp[tempCounter] = elements[index];
+            tempCounter++;
+        }
+        elements = temp;
+        numberOfElements--;
+    }
+
+    public String get(int index) {
+        if (index >= 0 && index < elements.length) {
+            return elements[index];
+        } else {
+            throw new ArrayIndexOutOfBoundsException("Out of Range");
+        }
+    }
+
+    public int get(String element) {
+        for (int i = 0; i < elements.length; i++) {
+            if (element.equalsIgnoreCase(elements[i])) return i;
+        }
+        return 0;
+    }
+
+    public int size() {
+        return numberOfElements;
+    }
 }
--- tests/ArrayListTest.java
+++ tests/ArrayListTest.java
@@ -1,2 +1,219 @@
-package PACKAGE_NAME;public class ArrayListTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayListTest {
+
+    private ArrayList myStringArray;
+
+    @BeforeEach
+    public void initializeArray(){
+        myStringArray = new ArrayList();
+    }
+
+    @Test
+    public void ArrayListIsEmpty(){
+        assertTrue(myStringArray.isEmpty());
+    }
+
+    @Test
+    public void ArrayListIsEmpty_ElementCanBeAdded(){
+        assertTrue(myStringArray.isEmpty());
+
+        myStringArray.add("Bolaji");
+        assertFalse(myStringArray.isEmpty());
+    }
+
+    @Test
+    public void ArrayListISEmpty_TwoElementsCanBeAdded(){
+        assertTrue(myStringArray.isEmpty());
+
+        myStringArray.add("Bolai");
+        myStringArray.add("Mohammed");
+        assertFalse(myStringArray.isEmpty());
+    }
+
+    @Test
+    public void addOneElement_removeElement_listIsEmpty(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Bolaji");
+        assertFalse(myStringArray.isEmpty());
+
+        myStringArray.remove("Bolaji");
+        assertTrue(myStringArray.isEmpty());
+    }
+
+    @Test
+    public void addTwoElement_removeOne_listNotEmpty(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Bolaji");
+        myStringArray.add("Moh");
+
+        myStringArray.remove("Bolai");
+        assertFalse(myStringArray.isEmpty());
+    }
+
+    @Test
+    public void removeElementFromEmptyList_throwException(){
+        assertTrue(myStringArray.isEmpty());
+        assertThrows(ArrayIndexOutOfBoundsException.class, ()->myStringArray.remove("Moh"));
+    }
+
+    @Test
+    public void addOneElement_getByIndex_returnElement(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Bolaji");
+        assertEquals("Bolaji", myStringArray.get(0));
+    }
+
+    @Test
+    public void addTwoElements_getByIndex_returnElement(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Moh");
+        myStringArray.add("Beejay");
+
+        assertEquals("Beejay", myStringArray.get(1));
+        assertEquals("Moh", myStringArray.get(0));
+    }
+
+    @Test
+    public void addOneElement_getByElement_returnIndex(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Bolaji");
+        assertEquals(0, myStringArray.get("Bolaji"));
+    }
+
+    @Test
+    public void addTwoElements_getByElements_returnIndex(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Moh");
+        myStringArray.add("Beejay");
+
+        assertEquals(1, myStringArray.get("Beejay"));
+        assertEquals(0, myStringArray.get("Moh"));
+    }
+
+    @Test
+    public void addThreeElements_removeMiddleElement_elementThreeMovesToTwo(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Moh");
+        myStringArray.add("Beejay");
+        myStringArray.add("Jumoke");
+        assertEquals(1, myStringArray.get("Beejay"));
+
+        myStringArray.remove("Beejay");
+        assertEquals(1, myStringArray.get("Jumoke"));
+    }
+
+    @Test
+    public void addThreeElements_removeFirstTwo_element3MovesTo1(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Moh");
+        myStringArray.add("Beejay");
+        myStringArray.add("Jumoke");
+        assertEquals(2, myStringArray.get("Jumoke"));
+
+        myStringArray.remove("Beejay");
+        myStringArray.remove("Moh");
+        assertEquals(0, myStringArray.get("Jumoke"));
+    }
+
+    @Test
+    public void addElement_getWithCaseIgnore_returnIndex(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Moh");
+        myStringArray.add("Beejay");
+        myStringArray.add("Jumoke");
+
+        assertEquals(1, myStringArray.get("beejay"));
+        assertEquals(2, myStringArray.get("JUMOKE"));
+    }
+
+    @Test
+    public void addOneElement_getWrongIndex_THrowException(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Bolaji");
+
+        assertEquals("Bolaji", myStringArray.get(0));
+        assertThrows(ArrayIndexOutOfBoundsException.class, ()->myStringArray.get(5));
+    }
+
+    @Test
+    public void canAddFiveElementsInList(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Bolaji");
+        myStringArray.add("Moh");
+        myStringArray.add("Jumoke");
+        myStringArray.add("Orisha");
+        myStringArray.add("Izu");
+
+        assertEquals(0, myStringArray.get("Bojaji"));
+        assertEquals(1, myStringArray.get("Moh"));
+        assertEquals(2, myStringArray.get("Jumoke"));
+        assertEquals(3, myStringArray.get("Orisha"));
+        assertEquals(4, myStringArray.get("Izu"));
+    }
+
+    @Test
+    public void canAddFiveElements_getElementByIndex(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Bolaji");
+        myStringArray.add("Moh");
+        myStringArray.add("Jumoke");
+        myStringArray.add("Orisha");
+        myStringArray.add("Izu");
+
+        assertEquals("Orisha", myStringArray.get(3));
+    }
+
+    @Test
+    public void addFiveElements_RemoveTwo_listReNumbers(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Bolaji");
+        myStringArray.add("Moh");
+        myStringArray.add("Jumoke");
+        myStringArray.add("Orisha");
+        myStringArray.add("Izu");
+
+        myStringArray.remove("Izu");
+        myStringArray.remove("Jumoke");
+
+        assertEquals(0, myStringArray.get("Bolaji"));
+        assertEquals(1, myStringArray.get("Moh"));
+        assertEquals(2, myStringArray.get("Orisha"));
+    }
+
+    @Test
+    public void addTwoElements_getlistSize(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Moh");
+        myStringArray.add("Beejay");
+
+        assertEquals(2, myStringArray.size());
+    }
+
+    @Test
+    public void addFourElements_getListSize(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Moh");
+        myStringArray.add("Beejay");
+        myStringArray.add("Jumoke");
+        myStringArray.add("Orisha");
+
+        assertEquals(4, myStringArray.size());
+    }
+
+    @Test
+    public void addFourElements_removeTwo_getListSize(){
+        assertTrue(myStringArray.isEmpty());
+        myStringArray.add("Moh");
+        myStringArray.add("Beejay");
+        myStringArray.add("Jumoke");
+        myStringArray.add("Orisha");
+
+        myStringArray.remove("Moh");
+        myStringArray.remove("Beejay");
+        assertEquals(2, myStringArray.size());
+    }
 }
