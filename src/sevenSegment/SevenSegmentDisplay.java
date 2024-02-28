3d8cbe3b26fbe2b9b41d13e932a7824d570f1894
Dee-dayo <darhyor2050@gmail.com>
2024-02-28 22:33:15 +0100

    Seven segment

---
+++ src/sevenSegment/SevenSegmentDisplay.java
@@ -0,0 +1,51 @@
+package sevenSegment;
+
+public class SevenSegmentDisplay {
+
+    public static void displayScreen(String number) {
+        if (number.length() == 8) {
+            String[] num = number.split("");
+
+            if (num[7].equals("1")) {
+
+                if (num[0].equals("1"))
+                    System.out.println("######");
+                else
+                    System.out.println();
+
+                for (int index = 0; index < 2; index++) {
+                    if (num[5].equals("1"))
+                        System.out.print("#");
+                    else
+                        System.out.print(" ");
+                    if (num[1].equals("1"))
+                        System.out.println("    #");
+                    else
+                        System.out.println();
+                }
+
+                if (num[6].equals("1"))
+                    System.out.println("######");
+                else
+                    System.out.println();
+
+                for (int index = 0; index < 2; index++) {
+                    if (num[4].equals("1"))
+                        System.out.print("#");
+                    else
+                        System.out.print(" ");
+                    if (num[2].equals("1"))
+                        System.out.println("    #");
+                    else
+                        System.out.println(" ");
+                }
+
+                if (num[3].equals("1"))
+                    System.out.println("######");
+                else
+                    System.out.println();
+            }
+        }
+    }
+
+}
---
+++ tests/sevenSegmentTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static sevenSegment.SevenSegmentDisplay.displayScreen;
+
+public class sevenSegmentTest {
+    @Test
+    public static void main(String[] args) {
+        displayScreen("11111111");
+    }
+}
