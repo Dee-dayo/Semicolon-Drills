f16e3f92cb654d6e7648613b528857da41c98445
Dee-dayo <darhyor2050@gmail.com>
2024-03-07 05:34:50 +0100

    function calculate riders payment added

--- src/drills/BackToSenderLogistics.java
+++ src/drills/BackToSenderLogistics.java
@@ -2,16 +2,25 @@ package drills;
 
 public class BackToSenderLogistics {
     public static int calculateRidersPayment(int noOfDeliveries){
-        Integer noOfDeliveries1 = getCommissionPayment(noOfDeliveries);
-        if (noOfDeliveries1 != null) return noOfDeliveries1;
-        return 0;
-    }
-
-    private static Integer getCommissionPayment(int noOfDeliveries) {
         if (noOfDeliveries < 50) return noOfDeliveries * 160 + 5_000;
         if (noOfDeliveries <= 59) return noOfDeliveries * 200 + 5_000;
         if (noOfDeliveries <= 69) return noOfDeliveries * 250 + 5_000;
         if (noOfDeliveries <=100) return noOfDeliveries * 500 + 5_000;
-        return null;
+        return 0;
     }
+
+
+    public static int calculateRidersPaymentTakeTwo(int noOfDeliveries) {
+        int basePayment = 5000;
+        int perDeliveryPayment;
+
+        if (noOfDeliveries < 50) perDeliveryPayment = 160;
+        else if (noOfDeliveries <= 59) perDeliveryPayment = 200;
+        else if (noOfDeliveries <= 69) perDeliveryPayment = 250;
+        else if (noOfDeliveries <= 100) perDeliveryPayment = 500;
+        else return 0;
+
+        return noOfDeliveries * perDeliveryPayment + basePayment;
+    }
+
 }
--- tests/drillsTest/BackToSenderTest.java
+++ tests/drillsTest/BackToSenderTest.java
@@ -3,6 +3,7 @@ package drillsTest;
 import org.junit.jupiter.api.Test;
 
 import static drills.BackToSenderLogistics.calculateRidersPayment;
+import static drills.BackToSenderLogistics.calculateRidersPaymentTakeTwo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BackToSenderTest {
@@ -25,5 +26,10 @@ public class BackToSenderTest {
     @Test
     public void backToSenderTest4() {
         assertEquals(45_000, calculateRidersPayment(80));
+    }
+
+    @Test
+    public void backToSenderTest5() {
+        assertEquals(9_000, calculateRidersPaymentTakeTwo(25));
     }
 }
